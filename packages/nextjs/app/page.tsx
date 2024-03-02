e92c5ab5b956f60148c65fdf0b2aa2a0c9af1113
PraiseOmeire <praiseomeire99@gmail.com>
2024-03-02 14:48:45 +0100

    next update

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,25 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
-import Link from "next/link";
+import React from "react";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import ImageGallery from "~~/components/ImageGallery";
-import Modal from "~~/components/Modal";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-  const [modalOpen, setModalOpen] = useState(false);
-  const [selectedImageData, setSelectedImageData] = useState(null);
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const handleImageClick = (imageName: any) => {
-    setSelectedImageData(imageName);
-    setModalOpen(true);
-  };
-
   const images = [
     ,
     "anime4.png",
@@ -36,7 +21,7 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <ImageGallery images={images} handleImageClick={handleImageClick} />
+      <ImageGallery images={images} />
     </>
   );
 };
